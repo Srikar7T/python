036ec3011da7e56d06c182f85f9a1ec676cc394b
Srikar N <61531041+Srikar7T@users.noreply.github.com>
2023-12-26 16:33:17 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+import os
+list1=[5,8,7,6,3,2,1,4]
+list1.reverse()
+print("the reversed list is", list1)
+
+list2=[9,8,7,6,5,4,3,2,1]
+list2.sort()
+print("the sorted list is", list2)
+
+list3=[]
+list3=list1.copy()
+print("updated list is",list3)
+
+index_values=list2[2:6]
+print("index values are",index_values)
